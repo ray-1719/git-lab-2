91aa2faf512cb67bcea9818555ebd5adaccbb5fb
Raymond Lawson <rl645817@ohio.edu>
2019-01-22 11:12:14 -0500

    update program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Raymond Lawson>
+ *        Date: <Jan 22 2019>
+ * Description: Code for Lab2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
